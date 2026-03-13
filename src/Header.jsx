2ca0d145ae192249addd1a88d388f7f26aa23f25
Jdly <jadetudtud1@gmail.com>
2026-03-13 09:05:56 +0800

    Youtube Header link updated
    blahblahblag

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
         <div className="navbar bg-blue-500 min-h-8 h-8 text-sm height-10">
             <nav>
                 <ul className="flex space-x-4">
-                    <li><a href="https://www.youtube.com/@Chisey">YouTube</a></li>
+                    <li><a href="https://youtu.be/00Bwmbf3pOM?si=cQ-PT7AcMR9gK-Jq">YouTube</a></li>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
